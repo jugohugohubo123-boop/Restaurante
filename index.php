508b51ea00bf44b387188912bfa5be32d2589492
Rodriguez12 <vivejuegarie@gmail.com>
2026-03-31 10:23:49 -0500

    Inicio de Proyecto

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <title>Gourmet Criollo</title>
+</head>
+<body>
+    <div class="layout">
+        <header class="header">
+            <div class="header__contenido container">
+                <p class="header__titulo">Gourmet Criollo</p>
+                  <nav class="header__nav">
+                    <ul>
+                    <li><a href="#" >Inicio</a></li>
+                    <li><a href="#" >Carta</a></li>
+                    <li><a href="#" >Blog</a></li>
+                    <li><a href="#" >Contactos</a></li>
+                    </ul>
+                  </nav>
+                  <a class="cta-primary" href="#">Reservar Mesa </a>
+            </div>
+            </header>
+        
+        <main>
+            <p>
+                Contendo Principal
+            </p>
+        </main>
+<footer class="foooter">
+    <p>footer</footer></p>
+</foooter>
+</div>
+</body>
+</html>
